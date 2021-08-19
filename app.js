a24c796e8a7bbb9da5945af59d8b2004e3c8bcb5
Sumit Chavhan <srchavhan29@gmail.com>
2021-08-19 17:18:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL= "https://api.funtranslations.com/translate/dothraki.json"
+
+function getTranslationURL(input){
+   return serverURL + "?" + "text=" + input
+}
+
+
+function clickHandler(){
+var inputText = txtInput.value;
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+   var translatedText = json.contents.translated;
+   outputDiv.innerText = translatedText;
+
+})
+};
+
+btnTranslate.addEventListener("click", clickHandler)
